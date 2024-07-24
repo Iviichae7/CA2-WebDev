4b9bfb7780c80f2e17e089c9ed503c6b681ec330
iviichae7 <iviichae7@gmail.com>
2024-07-24 22:51:18 +0100

    U: Adjustments to the project packages, removal of hmtl - no more needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./views/**/*.ejs", "./public/**/*.js", "./public/**/*.css"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: "#F9F1F0",
+        roseQuartz: "#FADCD9",
+        dustyRose: "#F8AFA6",
+        coral: "#F79489",
+      },
+      keyframes: {
+        fadeInChar: {
+          "0%": { opacity: "0", transform: "translateX(-50%)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+        fadeInForm: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeInChar: "fadeInChar 0.5s ease-in-out forwards",
+        fadeInForm: "fadeInForm 2s ease-in-out forwards",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/typography")],
+};
