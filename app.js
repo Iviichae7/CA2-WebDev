5ee93123bbedf31cc31c485b042cc7d51eb9d7fa
iviichae7 <iviichae7@gmail.com>
2024-07-27 22:22:42 +0100

    U: Refactor app for improved code structure and readability

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+require("dotenv").config({ path: ".env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const promisePool = require("./config/database");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
 const app = express();
 const port = 3000;
+
+const sessionStore = new MySQLStore({}, promisePool);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -12,21 +17,54 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: { secure: false },
+  })
+);
+
+// Auth routes
+app.use("/auth", require("./routes/auth"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/submit", async (req, res) => {
-  const { first_name, surname, username, email } = req.body;
+const { getTasks, createTask } = require("./models/Task");
+
+app.get("/tasks/:userId", async (req, res) => {
+  const { userId } = req.params;
   try {
-    const [result] = await promisePool.query(
-      "INSERT INTO Users (first_name, surname, username, email) VALUES (?, ?, ?, ?)",
-      [first_name, surname, username, email]
-    );
-    res.redirect(`/tasks/${result.insertId}`);
+    const [user] = await promisePool.query("SELECT * FROM Users WHERE id = ?", [
+      userId,
+    ]);
+    const tasks = await getTasks(userId);
+    res.render("tasks", { user: user[0], tasks });
   } catch (err) {
-    res.status(500).send("Error adding user");
+    res.status(500);
+  }
+});
+
+// Task route
+app.post("/tasks", async (req, res) => {
+  const { taskName, description, deadline } = req.body;
+  const userId = req.session.userId;
+
+  if (!userId) {
+    return res.status(400);
+  }
+
+  try {
+    const taskId = await createTask(userId, taskName, description, deadline);
+    res.json({ taskId });
+  } catch (err) {
+    res.status(500);
   }
 });
 
