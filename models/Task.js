c1517c340784f2ba6dde4cc4cdd149723ba11729
iviichae7 <iviichae7@gmail.com>
2024-07-27 22:19:10 +0100

    U: improved code structure and readability

--- models/Task.js
+++ models/Task.js
@@ -8,26 +8,18 @@ async function getTasks(userId) {
     );
     return tasks;
   } catch (err) {
-    console.error("Error fetching tasks:", err);
     throw err;
   }
 }
 
 async function createTask(userId, taskName, description, deadline) {
-  console.log(
-    `Creating task for userId: ${userId}, taskName: ${taskName}, description: ${description}, deadline: ${deadline}`
-  );
   try {
     const [result] = await promisePool.query(
       "INSERT INTO Tasks (user_id, task_name, description, deadline) VALUES (?, ?, ?, ?)",
       [userId, taskName, description, deadline]
     );
-    console.log(
-      `Task created for user ID: ${userId} with task ID: ${result.insertId}`
-    );
     return result.insertId;
   } catch (err) {
-    console.error("Error creating task:", err);
     throw err;
   }
 }
