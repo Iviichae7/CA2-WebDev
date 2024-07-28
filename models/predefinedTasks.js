9c39c798af6fcbb47b98f047485c3c0149b90549
iviichae7 <iviichae7@gmail.com>
2024-07-28 20:24:22 +0100

    A: Tasks module

---
+++ models/predefinedTasks.js
@@ -0,0 +1,35 @@
+const predefinedTasks = [
+  {
+    task_name: "Study for Exam",
+    description:
+      "Review chapters and complete practice problems to prepare for the upcoming exam.",
+    deadline: null,
+  },
+  {
+    task_name: "Complete Continuous Assessment",
+    description: "Finish and submit the continuous assessment",
+    deadline: null,
+  },
+  {
+    task_name: "Revision of Lecture",
+    description: "Go over notes and slides from the recent lecture.",
+    deadline: null,
+  },
+  {
+    task_name: "Prepare for Quiz",
+    description: "Study chapters and ensure understanding of the key concepts.",
+    deadline: null,
+  },
+  {
+    task_name: "Complete Lab",
+    description: "Write and submit the lab report for the recent class.",
+    deadline: null,
+  },
+  {
+    task_name: "Group Study Session",
+    description: "Attend the group study session.",
+    deadline: null,
+  },
+];
+
+module.exports = predefinedTasks;
